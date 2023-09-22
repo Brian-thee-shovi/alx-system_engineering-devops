4ce81e9496ce0e5290203b32a88c0b435d8003a7
Brian-thee-shovi <brian14198@gmail.com>
2023-09-22 07:12:58 -0700

    my puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -6,20 +6,4 @@ file { 'school':
   mode    => '0744',
   path    => '/tmp/school',
   group   => 'www-data',
-}# Create a file in /tmp
-
-file { 'school':
-  content => 'I love Puppet',
-  owner   => 'www-data',
-  mode    => '0744',
-  path    => '/tmp/school',
-  group   => 'www-data',
-}# Create a file in /tmp
-
-file { 'school':
-  content => 'I love Puppet',
-  owner   => 'www-data',
-  mode    => '0744',
-  path    => '/tmp/school',
-  group   => 'www-data',
 }
