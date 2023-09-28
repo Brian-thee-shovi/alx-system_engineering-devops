59c6c6b3da81709fbd3ab315a8d91185a6fbd733
Brian-thee-shovi <brian14198@gmail.com>
2023-09-27 18:03:38 -0700

    7-puppet_install_nginx_web_server.pp innit

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -10,6 +10,7 @@ file { '/var/www/html/index.html':
 
 file_line { 'redirection-301':
   ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
 }
