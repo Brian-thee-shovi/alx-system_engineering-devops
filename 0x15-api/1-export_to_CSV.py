af7495e7fd4f371bf6ace695a297298cebd911aa
Brian-thee-shovi <brian14198@gmail.com>
2023-10-25 10:06:39 -0700

    updates innit

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,21 +8,21 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        userid = sys.argv[1]
+        user_id = sys.argv[1]
 
         apiurl = 'https://jsonplaceholder.typicode.com/'
-        userurl = apiurl + 'user/{}'.format(userid)
-        user_todos = apiurl + 'users/{}/todos'.format(userid)
+        userurl = apiurl + 'user/{}'.format(user_id)
+        user_todos = apiurl + 'users/{}/todos'.format(user_id)
 
         # requests data
         user = requests.get(userurl).json()
         todos = requests.get(user_todos).json()
 
         csv_data = ['"{}","{}","{}","{}"'.format(
-            userid,
+            user_id,
             user.get('username'),
             todo.get('completed'),
             todo.get('title')) for todo in todos]
 
-        with open('{}.csv'.format(userid), 'w') as csvf:
+        with open('{}.csv'.format(user_id), 'w') as csvf:
             csvf.write('\n'.join(csv_data))
