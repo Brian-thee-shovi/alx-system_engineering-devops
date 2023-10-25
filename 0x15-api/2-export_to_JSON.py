9e4f538cfd42b13ac92bf5dcbe1c9e0774d11040
Brian-thee-shovi <brian14198@gmail.com>
2023-10-25 09:26:14 -0700

    2-export_to_JSON.py done

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+converts user TODO list as to json data
+"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        userid = sys.argv[1]
+
+        apiurl = 'https://jsonplaceholder.typicode.com/'
+        userurl = apiurl + 'users/{}'.format(userid)
+        user_todos = apiurl + 'users/{}/todos'.format(userid)
+
+        #request data
+        user = requests.get(userurl).json()
+        todos = requests.get(user_todos).json()
+
+        json_data = [
+                {
+                    "task": todo.get('title'),
+                    "completed": todo.get('completed'),
+                    "username": user.get('username')
+                }
+                for todo in todos]
+        with open('{}.json'.format(userid), 'w') as jsonf:
+            json.dump({userid: json_data}, jsonf)
